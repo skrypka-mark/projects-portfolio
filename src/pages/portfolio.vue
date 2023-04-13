18821386aed57cb677c70020705c290188516254
divineript <divineript@gmail.com>
2023-04-13 17:48:57 +0300

    implemented portfolio and project pages

--- src/pages/portfolio.vue
+++ src/pages/portfolio.vue
@@ -1,7 +1,88 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div class="container">
+      <div class="projects-container">
+        <a href="" class="image-link">
+          <img class="image-project" src="../img/Project1.png" alt="project1">
+        </a>
+        <p class="title-project">Name project</p>
+      </div>
+
+      <div class="projects-container">
+        <a href="" class="image-link">
+          <img class="image-project" src="../img/Project2.png" alt="project2">
+        </a>
+        <p class="title-project">Name project</p>
+      </div>
+
+      <div class="projects-container">
+        <a href="" class="image-link">
+          <img class="image-project" src="../img/Project3.png" alt="project3">
+        </a>
+        <p class="title-project">Name project</p>
+      </div>
+
+      <div class="projects-container">
+        <a href="" class="image-link">
+          <img class="image-project" src="../img/Project4.png" alt="project4">
+        </a>
+        <p class="title-project">Name project</p>
+      </div>
+
+      <div class="projects-container">
+        <a href="" class="image-link">
+          <img class="image-project" src="../img/Project5.png" alt="project5">
+        </a>
+        <p class="title-project">Name project</p>
+      </div>
+
+      <div class="projects-container">
+        <a href="" class="image-link">
+          <img class="image-project" src="../img/Project6.png" alt="project6">
+        </a>
+        <p class="title-project">Name project</p>
+      </div>
+      <div class="projects-container">
+        <a href="" class="image-link">
+          <img class="image-project" src="../img/Project1.png" alt="project1">
+        </a>
+        <p class="title-project">Name project</p>
+      </div>
+
+      <div class="projects-container">
+        <a href="" class="image-link">
+          <img class="image-project" src="../img/Project2.png" alt="project2">
+        </a>
+        <p class="title-project">Name project</p>
+      </div>
+
+      <div class="projects-container">
+        <a href="" class="image-link">
+          <img class="image-project" src="../img/Project3.png" alt="project3">
+        </a>
+        <p class="title-project">Name project</p>
+      </div>
+
+      <div class="projects-container">
+        <a href="" class="image-link">
+          <img class="image-project" src="../img/Project4.png" alt="project4">
+        </a>
+        <p class="title-project">Name project</p>
+      </div>
+
+      <div class="projects-container">
+        <a href="" class="image-link">
+          <img class="image-project" src="../img/Project5.png" alt="project5">
+        </a>
+        <p class="title-project">Name project</p>
+      </div>
+
+      <div class="projects-container">
+        <a href="" class="image-link">
+          <img class="image-project" src="../img/Project6.png" alt="project6">
+        </a>
+        <p class="title-project">Name project</p>
+      </div>
+    </div>
 </template>
 
 <style>
@@ -11,5 +92,29 @@
     display: flex;
     align-items: center;
   }
+}
+
+.container {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  max-width: 1040px;
+  margin: 40px 40px 0 40px;
+}
+
+.projects-container {
+  margin-bottom: 25px;
+}
+
+.image-link {
+  display: block;
+  width: 330px;
+  height: 205px;
+}
+
+.image-project {
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
 }
 </style>
--- src/pages/project.vue
+++ src/pages/project.vue
@@ -3,5 +3,112 @@
 
 <template>
   <main>
+    <div class="container">
+      <div class="gallery">
+        <img src="../img/Project1.png" alt="" class="title-image-project">
+        <div class="image-collage">
+          <img src="../img/Project3.png" alt="" class="gallery-image-project">
+          <img src="../img/Project3.png" alt="" class="gallery-image-project">
+          <img src="../img/Project3.png" alt="" class="gallery-image-project">
+          <img src="../img/Project3.png" alt="" class="gallery-image-project">
+        </div>
+      </div>
+
+      <div class="about-project">
+        <div class="info-content">
+          <div class="points">
+          <ul>
+            <li class="point-list">GitHub</li>
+            <li class="point-list">Figma</li>
+            <li class="point-list">Tech Stack</li>
+            <li class="point-list">Description</li>
+          </ul>
+          <a href="">Open website</a>
+        </div>
+
+        <div class="project-info">
+          <a href="https://github.com/test-project.git">https://github.com/test-project.git</a>
+          <a href="https://www.figma.com/file/Wk2jbvoalPVHTLaLdLwlu2/Our-portfolio-project?node-id=1%3A46&t=NyiY0AegEXjXSmoY-1">https://www.figma.com/file/Wk2jbvoalsdhh323h76f6fnjjs...</a>
+          <p>Vue.js, NUXT, SCSS, NodeJs</p>
+          <p>MakerPlace is a platform that allows users to buy and sell 3D models online. The marketplace connects buyers and sellers in a secure and efficient way, allowing for easy transactions and reliable delivery of 3D models. <br> Users can create an account and upload their own 3D models for sale. They can set the price, description, and specifications of their models. Buyers can browse through the available 3D models, view images and specifications, and purchase the ones that they want.</p>
+        </div>
+        </div>
+      </div>
+
+    </div>
   </main>
 </template>
+
+<style>
+
+.container {
+  width: 1040px;
+  margin: 40px 40px 0 40px;
+}
+
+.gallery {
+  width: 430px;
+  height: 550px;
+}
+
+.title-image-project {
+  width: 100%;
+  height: 250px;
+  border-radius: 10px;
+  margin-bottom: 20px;
+}
+
+.image-collage {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.gallery-image-project {
+  width: 205px;
+  height: 130px;
+  margin-bottom: 20px;
+  border-radius: 10px;
+}
+
+.about-project {
+  width: 580px;
+}
+
+.info-content {
+  display: flex;
+  justify-content: space-between;
+}
+
+.point-list {
+  font-size: 14px;
+  font-style: 600;
+  color: #fff;
+  margin-bottom: 8px;
+}
+
+.project-info {
+  width: 80%;
+}
+
+.project-info a {
+  font-size: 14px;
+  color: #5BA8DF;
+  font-style: 400;
+  margin-bottom: 10px;
+}
+
+.project-info p {
+  font-size: 14px;
+  color: #fff;
+  opacity: 75%;
+  font-style: 400;
+  margin-bottom: 10px;
+}
+
+.project-info:last-child {
+  height: 155px;
+  overflow: hidden;
+}
+
+</style>
